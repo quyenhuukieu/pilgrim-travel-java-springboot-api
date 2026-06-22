43e0afca4e3deb9df8c7b6b27f88e5ac6203000b
Quyen Kieu <9664014+quyenhuukieu@users.noreply.github.com>
2026-06-22 06:09:57 -0500

    Add GitHub Actions Maven build pipeline

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO itineraries
+    (user_id, destination, date, completed)
+VALUES
+    (1, 'Japan - Tokyo', '2026-04-10', false);
+
+
+INSERT INTO itineraries
+    (user_id, destination, date, completed)
+VALUES
+    (1, 'Italy - Rome', '2026-06-15', true);
+
+
+INSERT INTO itineraries
+    (user_id, destination, date, completed)
+VALUES
+    (2, 'France - Paris', '2026-07-20', false);
+
+
+INSERT INTO itineraries
+    (user_id, destination, date, completed)
+VALUES
+    (2, 'Spain - Barcelona', '2026-08-05', false);
